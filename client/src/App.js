3c56222df29dc497a63571ee3b6d5721e0e86337
salasrob <roberjsalas1@gmail.com>
2021-10-15 17:51:12 -0500

    image bug on POST bugfix

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ const App = () => {
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className={classes.heading}  variant="h2" align="center">Memories</Typography>
-                <img className={classes.image}  src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -87,7 +87,7 @@ const Form = ({currentId, setCurrentId}) => {
                  <FileBase
                  type="file"
                  multiple={false}
-                 onDone={(base64)=> {setPostData({...postData, selectedFile:base64})}}/>
+                 onDone={({base64})=> {setPostData({...postData, selectedFile:base64})}}/>
                  
                  </div>
                  <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
